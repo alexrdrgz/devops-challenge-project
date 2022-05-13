2d371b6a5e86ac04e21321dd05e51552837a7d86
JGustavo11 <jgustavoht@gmail.com>
2022-05-13 11:25:08 -0700

    adds(main.tf): Adds blank main.tf with resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,37 @@
-// Here is where you should configure your remote state
+#Every block should be empty
 terraform {
-  backend {}
-
-  required_providers {
-    aws = {
-      souce = "hashicorp/aws"
-    }
+  backend "s3" {
+   
   }
 }
 
-// We will be using AWS for this challenge
+#Let them play with profile, make configurable values, a variable. like profile. it should be var.aws_profile. 
 provider "aws" {
-  profile = var.aws_profile
-  region  = var.aws_region
+  
+
+}
+
+
+#Here you will configure your security group to allow SSH on port 22 from ONLY your IP address.
+
+resource "aws_security_group" "allow_ssh" {
+ 
+
+}
+# #Here You will be creating the EC2 server. 
+resource "aws_instance" "web" {
+  
+}
+
+
+#Create S3 
+resource "aws_s3_bucket" "bucket_creation" {
+
+
+}
+
+#Create route 53 A record to point to IP of EC2
+resource "aws_route53_record" "www" {
+
 }
 
