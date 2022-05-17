b242d43de26ac5d85057fc5dc1afe1ab81fc5794
alexrdrgz <alejandro.xsc@gmail.com>
2022-05-17 15:46:20 -0700

    feat(main.tf): created vpc and security group rule to allow ssh into port 22

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,56 @@
-// Here is where you should configure your remote state
+#Here you will configure your terraform and backend 
 terraform {
-  backend {}
-
-  required_providers {
-    aws = {
-      souce = "hashicorp/aws"
-    }
+  backend "s3" {
+   
   }
 }
 
-// We will be using AWS for this challenge
+# Here you will configure your provider (AWS)
 provider "aws" {
-  profile = var.aws_profile
-  region  = var.aws_region
+  
+
 }
 
+
+#Here you will configure your security group to allow SSH on port 22 from ONLY your IP address.
+resource "aws_security_group" "allow_ssh" {
+  name = "alex-security-group"
+  description = "seccurity group that allows you to ssh to port 22 from your personal IP"
+  vpc_id = aws_vpc.main.id
+
+
+}
+
+resource "aws_security_group_rule" "private_ssh_inbound_rule" {
+  type = ingress
+  from_port = 0
+  to_port = 22
+  protocol = "-1"
+  cidr_blocks = [ "76.224.230.251" ]
+
+  security_group_id = aws_security_group.allow_ssh.id
+}
+
+# #Here You will be creating the EC2 server. 
+resource "aws_instance" "web" {
+  vpc_security_group_ids = [
+      "Default VPC security group here",
+  ]
+}
+
+root_block_device {
+  #Fill out the volume size and storage in here.
+}
+
+
+#Create S3 
+resource "aws_s3_bucket" "bucket_creation" {
+
+
+}
+
+#Create route 53 A record to point to IP of EC2
+resource "aws_route53_record" "www" {
+  zone_id = "You can find this in Route 53 service"
+
+}
