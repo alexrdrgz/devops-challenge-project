cbe463f3dc86e24d7c8d7e0c38782275336b1660
JGustavo11 <jgustavoht@gmail.com>
2022-05-17 10:50:56 -0700

    docs(all files): Fixes styling

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,7 +19,14 @@ resource "aws_security_group" "allow_ssh" {
 }
 # #Here You will be creating the EC2 server. 
 resource "aws_instance" "web" {
-  
+  resource "aws_instance" "web" {
+  vpc_security_group_ids = [
+      "Default VPC security group here",
+  ]
+
+  root_block_device {
+    #Fill out the volume size and storage in here.
+  }
 }
 
 
@@ -31,6 +38,7 @@ resource "aws_s3_bucket" "bucket_creation" {
 
 #Create route 53 A record to point to IP of EC2
 resource "aws_route53_record" "www" {
+  zone_id = "You can find this in Route 53 service"
 
 }
 
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,3 @@
-variable "aws_profile" {
-  description = "This variable should represent the profile configured on your local machine after running aws configure"
-}
-
 variable "aws_region" {
   description = "This variable represents the AWS region where all of the resources Terraform provisions will live, we normally default this to us-west-2 as it is cheaper than us-west-1"
   default     = "us-west-2"
