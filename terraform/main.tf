02d8988ee6cdd7037ad9859750df888312477dee
alexrdrgz <alejandro.xsc@gmail.com>
2022-05-18 18:21:32 -0700

    feat(main.tf): built rout53, aws_instance, security group resource

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,7 +18,8 @@ resource "aws_security_group" "allow_ssh" {
   description = "seccurity group that allows you to ssh to port 22 from your personal IP"
   vpc_id = aws_vpc.main.id
 
-
+  # Not sure if its better to create the rule in here? 
+  # ex. ingress {....}   
 }
 
 resource "aws_security_group_rule" "private_ssh_inbound_rule" {
@@ -33,8 +34,17 @@ resource "aws_security_group_rule" "private_ssh_inbound_rule" {
 
 # #Here You will be creating the EC2 server. 
 resource "aws_instance" "web" {
+  count = 1
+  instance_type = t3.micro
+  security_groups = [ "allow_ssh" ]
+  key_name = "alex-devops-challenge"
+  volume_size = 10
+  volume_type = standard
+  iam_instance_profile = "arn:aws:iam::961064903225:role/S3ReadWrite"
+  
+
   vpc_security_group_ids = [
-      "Default VPC security group here",
+      "sg-0e85eb9b181e03ab0",
   ]
 }
 
@@ -45,12 +55,14 @@ root_block_device {
 
 #Create S3 
 resource "aws_s3_bucket" "bucket_creation" {
-
-
+    bucket = "ar-s3bucket-challenge"
 }
 
 #Create route 53 A record to point to IP of EC2
 resource "aws_route53_record" "www" {
-  zone_id = "You can find this in Route 53 service"
-
+  zone_id = ""
+  name = "ar.bwtcdevopschallenge.com"
+  type = "A"
+  ttl = 300
+  records = [aws_instance.web.public_ip]
 }
